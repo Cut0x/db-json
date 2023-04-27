dbcb22032cca5d4f980e993c2ba04b554fe9796f
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-27 17:08:36 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+const module = require('./data_module');
+
+const file = './data.json';
+
+// Ajout d'une nouvelle entrée
+module.add_entry(file, { id: 1, name: 'Martin', age: 20 });
+
+// Modification d'une entrée existante
+module.update_entry(file, 1, { name: 'Martin', age: 25 });
+
+// Suppression d'une entrée existante
+module.delete_entry(file, 1);
+
+// Affichage des données actuelles
+const data = data_module.load_data(FILE_PATH);
+console.log(data);
