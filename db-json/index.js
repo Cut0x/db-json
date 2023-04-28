d7946060a3913b76d753b9264bc78e5091ccfdd9
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-29 01:00:30 +0200

    Patch

--- db-json/index.js
+++ db-json/index.js
@@ -1,40 +1,72 @@
 const fs = require('fs');
 
 function load_data(file_path) {
-    try {
-        const data = fs.readFileSync(file_path);
-        return JSON.parse(data);
-    } catch (error) {
-        return [];
-    }
-}
+    fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+        if (err) {
+            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
+        } else {
+            try {
+                const data = fs.readFileSync(file_path);
+                return JSON.parse(data);
+            } catch (error) {
+                return [];
+            };
+        };
+    });
+};
 
 function saveData(file_path, data) {
-    fs.writeFileSync(file_path, JSON.stringify(data));
-}
+    fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+        if (err) {
+            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
+        } else {
+            fs.writeFileSync(file_path, JSON.stringify(data));
+        };
+    });
+};
 
 function add_entry(file_path, new_entry) {
-    const data = load_data(file_path);
-    data.push(new_entry);
-    saveData(file_path, data);
-}
+    fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+        if (err) {
+            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
+        } else {
+            const data = load_data(file_path);
+            data.push(new_entry);
+            saveData(file_path, data);
+        };
+    });
+};
 
 function update_entry(file_path, entry_id, updated_entry) {
-    const data = load_data(file_path);
-    const index = data.findIndex(entry => entry.id === entry_id);
-    if (index !== -1) {
-        data[index] = { ...data[index], ...updated_entry };
-        saveData(file_path, data);
-    }
-}
+    fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+        if (err) {
+            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
+        } else {
+            const data = load_data(file_path);
+            const index = data.findIndex(entry => entry.id === entry_id);
+
+            if (index !== -1) {
+                data[index] = { ...data[index], ...updated_entry };
+                saveData(file_path, data);
+            };
+        };
+    });
+};
 
 function delete_entry(file_path, entry_id) {
-    const data = load_data(file_path);
-    const index = data.findIndex(entry => entry.id === entry_id);
-    if (index !== -1) {
-        data.splice(index, 1);
-        saveData(file_path, data);
-    }
-}
+    fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+        if (err) {
+            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
+        } else {
+            const data = load_data(file_path);
+            const index = data.findIndex(entry => entry.id === entry_id);
+
+            if (index !== -1) {
+                data.splice(index, 1);
+                saveData(file_path, data);
+            };
+        };
+    });
+};
 
 module.exports = { load_data, add_entry, update_entry, delete_entry };
--- test.js
+++ test.js
@@ -1,16 +1,23 @@
 const tmodule = require('./db-json/index');
-
 const file = './data.json';
 
 // Ajout d'une nouvelle entrée
+console.log('Avant l\'ajout :');
+console.log(tmodule.load_data(file));
 tmodule.add_entry(file, { id: 1, name: 'Martin', age: 20 });
+console.log('Après l\'ajout :');
+console.log(tmodule.load_data(file));
 
 // Modification d'une entrée existante
+console.log('Avant la modification :');
+console.log(tmodule.load_data(file));
 tmodule.update_entry(file, 1, { name: 'Martin', age: 25 });
+console.log('Après la modification :');
+console.log(tmodule.load_data(file));
 
 // Suppression d'une entrée existante
+console.log('Avant la suppression :');
+console.log(tmodule.load_data(file));
 tmodule.delete_entry(file, 1);
-
-// Affichage des données actuelles
-const data = tmodule.load_data(FILE_PATH);
-console.log(data);
+console.log('Après la suppression :');
+console.log(tmodule.load_data(file));
