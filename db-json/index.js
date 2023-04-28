8e2be915e9dfdef0d8593b5e1792a1ecdc0fc252
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-29 01:15:16 +0200

    all refonte

--- db-json/index.js
+++ db-json/index.js
@@ -1,81 +1,87 @@
 const fs = require('fs');
 
-function load_data(file_path) {
-    fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
-        if (err) {
-            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
-        } else {
-            try {
-                const data = fs.readFileSync(file_path);
-                return JSON.parse(data);
-            } catch (error) {
-                return [];
-            };
-        };
-    });
-};
+function loadData(file_path) {
+    const data = fs.readFileSync(file_path, { encoding: 'utf8', flag: 'r' });
+    try {
+        return JSON.parse(data);
+    } catch (err) {
+        throw new TypeError(`Le fichier ${file_path} ne contient pas de données JSON valides.`);
+    }
+}
 
 function saveData(file_path, data) {
-    fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
-        if (err) {
-            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
-        } else {
-            fs.writeFileSync(file_path, JSON.stringify(data));
-        };
-    });
-};
+    try {
+        const json_data = JSON.stringify(data);
+        fs.writeFileSync(file_path, json_data, { encoding: 'utf8', flag: 'w' });
+    } catch (err) {
+        throw new TypeError(`Impossible d'écrire les données dans le fichier ${file_path}.`);
+    }
+}
 
-function add_entry(file_path, new_entry) {
+function addEntry(file_path, new_entry) {
     fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
         if (err) {
-            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
+            throw new TypeError(`Le fichier ${file_path} n'est pas accessible en lecture et/ou en écriture.`);
         } else {
-            let data = load_data(file_path);
-            if (!data) {
-                data = [];
-            };
+            const data = loadData(file_path);
+
+            if (!Array.isArray(data)) {
+                throw new TypeError(`Le fichier ${file_path} ne contient pas un tableau JSON valide.`);
+            }
+
+            const new_id = data.length > 0 ? data[data.length - 1].id + 1 : 1;
+            new_entry.id = new_id;
             data.push(new_entry);
-            saveData(file_path, data);
-        };
-    });
-};
 
-function update_entry(file_path, entry_id, updated_entry) {
+            saveData(file_path, data);
+        }
+    });
+}
+
+function updateEntry(file_path, updated_entry) {
     fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
         if (err) {
-            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
+            throw new TypeError(`Le fichier ${file_path} n'est pas accessible en lecture et/ou en écriture.`);
         } else {
-            let data = load_data(file_path);
+            const data = loadData(file_path);
 
-            if (!data) {
-                data = [];
-            };
+            if (!Array.isArray(data)) {
+                throw new TypeError(`Le fichier ${file_path} ne contient pas un tableau JSON valide.`);
+            }
 
-            const index = data.findIndex(entry => entry.id === entry_id);
+            const index = data.findIndex(entry => entry.id === updated_entry.id);
 
             if (index !== -1) {
-                data[index] = { ...data[index], ...updated_entry };
+                data[index] = updated_entry;
                 saveData(file_path, data);
-            };
-        };
+            } else {
+                throw new TypeError(`L'entrée avec l'ID ${updated_entry.id} n'existe pas dans le fichier ${file_path}.`);
+            }
+        }
     });
-};
+}
 
-function delete_entry(file_path, entry_id) {
+function deleteEntry(file_path, entry_id) {
     fs.access(file_path, fs.constants.R_OK | fs.constants.W_OK, (err) => {
         if (err) {
-            throw new TypeError(`Le fichier ${file} n'est pas accessible en lecture et/ou en écriture.`);
+            throw new TypeError(`Le fichier ${file_path} n'est pas accessible en lecture et/ou en écriture.`);
         } else {
-            const data = load_data(file_path);
-            console.log("Loaded data:", data);
+            const data = loadData(file_path);
+
+            if (!Array.isArray(data)) {
+                throw new TypeError(`Le fichier ${file_path} ne contient pas un tableau JSON valide.`);
+            }
+
             const index = data.findIndex(entry => entry.id === entry_id);
 
             if (index !== -1) {
                 data.splice(index, 1);
                 saveData(file_path, data);
-            };
-        };
+            } else {
+                throw new TypeError(`L'entrée avec l'ID ${entry_id} n'existe pas dans le fichier ${file_path}.`);
+            }
+        }
     });
-};
+}
 
-module.exports = { load_data, add_entry, update_entry, delete_entry };
+module.exports = { loadData, addEntry, updateEntry, deleteEntry };
--- test.js
+++ test.js
@@ -1,23 +1,23 @@
-const tmodule = require('./db-json/index');
+const { loadData, addEntry, updateEntry, deleteEntry } = require('./db-json/index');
 const file = './data.json';
 
 // Ajout d'une nouvelle entrée
 console.log('Avant l\'ajout :');
-console.log(tmodule.load_data(file));
-tmodule.add_entry(file, { id: 1, name: 'Martin', age: 20 });
+console.log(loadData(file));
+addEntry(file, { id: 1, name: 'Martin', age: 20 });
 console.log('Après l\'ajout :');
-console.log(tmodule.load_data(file));
+console.log(loadData(file));
 
 // Modification d'une entrée existante
 console.log('Avant la modification :');
-console.log(tmodule.load_data(file));
-tmodule.update_entry(file, 1, { name: 'Martin', age: 25 });
+console.log(loadData(file));
+updateEntry(file, 1, { name: 'Martin', age: 25 });
 console.log('Après la modification :');
-console.log(tmodule.load_data(file));
+console.log(loadData(file));
 
 // Suppression d'une entrée existante
 console.log('Avant la suppression :');
-console.log(tmodule.load_data(file));
-tmodule.delete_entry(file, 1);
+console.log(loadData(file));
+deleteEntry(file, 1);
 console.log('Après la suppression :');
-console.log(tmodule.load_data(file));
+console.log(loadData(file));
