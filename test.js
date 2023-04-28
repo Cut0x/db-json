ca163d5e89630329802760a18843c3106a0e5d0a
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-28 19:24:27 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
-const module = require('./db-json/index');
+const tmodule = require('./db-json/index');
 
 const file = './data.json';
 
 // Ajout d'une nouvelle entrée
-module.add_entry(file, { id: 1, name: 'Martin', age: 20 });
+tmodule.add_entry(file, { id: 1, name: 'Martin', age: 20 });
 
 // Modification d'une entrée existante
-module.update_entry(file, 1, { name: 'Martin', age: 25 });
+tmodule.update_entry(file, 1, { name: 'Martin', age: 25 });
 
 // Suppression d'une entrée existante
-module.delete_entry(file, 1);
+tmodule.delete_entry(file, 1);
 
 // Affichage des données actuelles
-const data = data_module.load_data(FILE_PATH);
+const data = tmodule.load_data(FILE_PATH);
 console.log(data);
